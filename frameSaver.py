47c2c4a11b5d11eed924963b90417c5df572bb50
Bruno Ćorić <46404490+JBear111@users.noreply.github.com>
2020-08-12 17:35:41 +0200

    Update frameSaver.py

--- frameSaver.py
+++ frameSaver.py
@@ -1,5 +1,4 @@
 import sys
-import argparse
 import os
 
 import cv2
@@ -17,25 +16,10 @@ def extractImages(pathIn, pathOut):
         vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line
         success,image = vidcap.read()
         print ('Read a new frame: ', success)
-        cv2.imwrite(os.path.join(pathOut,"{}-{}-{}.jpg".format(season,epizoda,count)), image)     #ime: ime npr. S1-ep1-1.jpg
-        count = count + 1   #count + 3 za svake 3 sekunde...
-
+        cv2.imwrite(os.path.join(pathOut,"{}-{}-{}.jpg".format(season,epizoda,count)), image)     #format: S1-ep1-1.jpg
+        count = count + 1   #every second saves a frame
+        
 if __name__=="__main__":
-    filename = os.listdir("Torrent/S1")
+    filename = os.listdir(PATH)
     for file in filename:
-        extractImages("Torrent/S1/" + file, "input/S1")
-
-    filename = os.listdir("Torrent/S1")
-    for file in filename:
-        extractImages("Torrent/S1/" + file, "input/S1")
-
-    filename = os.listdir("Torrent/S3")
-    for file in filename:
-        extractImages("Torrent/S3/"+ file, "input/S3")
-
-    filename = os.listdir("Torrent/S4")
-    for file in filename:
-        extractImages("Torrent/S4/" + file, "input/S4")
-
-#Torrent/S1/ep1.mkv
-#input/S1
+        extractImages(SOURCE + file, DEST)
